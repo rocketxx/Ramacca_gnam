7cb2091b336f234754eee16e6caaef260af1ac99
rocketxx <william97.ragusa@gmail.com>
2023-07-15 21:32:00 +0200

    Render bottone solo se esistono selezionati

--- src/components/ui/ListItemCondimenti.js
+++ src/components/ui/ListItemCondimenti.js
@@ -1,55 +1,56 @@
-// import { useEffect, useState } from 'react'
-// import {Text} from 'react-native'
-// import { getUserInfo } from '../../util/auth'
-// import Button from '../../components/ui/Button'
-// import { loadDataToFirebase } from '../../repository/repository'
-
-// function CartScreen()
-// {
-//     const [userInfo, setUserInfo] = useState({})
-//     useEffect(() => {
-//       const func = async () => {
-//         await getUserInfo().then((res) => {
-//           setUserInfo(res)
-//         })
-//       }
-//       func();
-//     }, [])
-//     return (
-//         <>
-//         <Text>Carrello Cliente</Text>
-//         <Text>{userInfo?.email}</Text>
-//         {/* <Button onPress={() => loadDataToFirebase()}>LOAD</Button> */}
-//         </>
-//     )
-// }
-
-// export default CartScreen;
-
-// import React from 'react';
-import { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserInfo } from '../../util/auth'
 import Button from '../../components/ui/Button';
 
-const ListItemCondimenti = ({info}) => {
-      const [userInfo, setUserInfo] = useState({})
-      const counters = useSelector(state => state.counters);
-      // console.log(counters)
-    useEffect(() => {
-      const func = async () => {
+const ListItemCondimenti = ({ info }) => {
+  const [userInfo, setUserInfo] = useState({});
+  const [selectedItems, setSelectedItems] = useState([]);
+  
+  useEffect(() => {
+    const func = async () => {
+      // Carica le informazioni dell'utente
+    };
+    func();
+  }, []);
 
-      }
-      func();
-    }, [])
+  const handleItemPress = (item) => {
+    // Aggiungi o rimuovi l'elemento selezionato dalla lista dei selezionati
+    if (selectedItems.some(selectedItem => selectedItem.id === item.id)) {
+      setSelectedItems(selectedItems.filter(selectedItem => selectedItem.id !== item.id));
+    } else {
+      setSelectedItems([...selectedItems, item]);
+    }
+  };
 
+  const handlePrintSelection = () => {
+    console.log(selectedItems);
+  };
+
+  // Dati di mock
+  const mockData = [
+    { id: 1, title: 'Condimento 1', description: 'Descrizione del condimento 1' },
+    { id: 2, title: 'Condimento 2', description: 'Descrizione del condimento 2' },
+    { id: 3, title: 'Condimento 3', description: 'Descrizione del condimento 3' },
+  ];
 
   return (
-
     <View style={styles.container}>
       <Text style={styles.title}>Lista Condimenti: {info}</Text>
-
+      {mockData.map(item => (
+        <TouchableOpacity
+          key={item.id}
+          style={[styles.itemContainer, selectedItems.some(selectedItem => selectedItem.id === item.id) && styles.selectedItem]}
+          onPress={() => handleItemPress(item)}
+        >
+          <View style={styles.itemContent}>
+            <Text style={styles.itemTitle}>{item.title}</Text>
+            <Text style={styles.itemDescription}>{item.description}</Text>
+          </View>
+        </TouchableOpacity>
+      ))}
+      {selectedItems.length > 0 && <Button onPress={handlePrintSelection}>Aggiungi All'Ordine</Button>}
     </View>
   );
 };
@@ -58,7 +59,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    paddingTop:150
+    paddingTop: 150,
   },
   title: {
     fontSize: 20,
@@ -67,18 +68,26 @@ const styles = StyleSheet.create({
   },
   itemContainer: {
     marginBottom: 8,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    backgroundColor: 'white',
+    elevation: 2, // Ombra
+  },
+  selectedItem: {
+    backgroundColor: 'lightgreen',
+  },
+  itemContent: {
+    padding: 16,
   },
   itemTitle: {
     fontSize: 18,
     fontWeight: 'bold',
+    marginBottom: 8,
   },
   itemDescription: {
     fontSize: 16,
     color: '#888',
-  },
-  itemQuantity: {
-    fontSize: 16,
-    color: 'blue',
   },
 });
 
